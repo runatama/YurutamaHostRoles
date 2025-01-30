da38f1b929fab52175baa809e351a20b319ec17f
KYMario <102628941+KYMario@users.noreply.github.com>
2025-01-30 22:20:31 +0900

    fix: setname常に送ってた事件

--- Modules/ExtendedPlayerControl.cs
+++ Modules/ExtendedPlayerControl.cs
@@ -267,7 +267,6 @@ namespace TownOfHost
 
             if (!force && Main.LastNotifyNames[(player.PlayerId, seer.PlayerId)] == name)
             {
-                Utils.aftersystemmeg = false;
                 //Logger.info($"Cancel:{player.name}:{name} for {seer.name}", "RpcSetNamePrivate");
                 return;
             }
