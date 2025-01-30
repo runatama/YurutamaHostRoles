09f096a35c963d70c3051a91ecde97b9d0226172
KYMario <102628941+KYMario@users.noreply.github.com>
2025-01-30 22:27:04 +0900

    ｽﾞﾐﾏｾﾞﾝ

--- Modules/ExtendedPlayerControl.cs
+++ Modules/ExtendedPlayerControl.cs
@@ -267,6 +267,7 @@ namespace TownOfHost
 
             if (!force && Main.LastNotifyNames[(player.PlayerId, seer.PlayerId)] == name)
             {
+                aftersystemmeg = true;
                 //Logger.info($"Cancel:{player.name}:{name} for {seer.name}", "RpcSetNamePrivate");
                 return;
             }
