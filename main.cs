abed8d606eb837042d26f1bbe1935b440cb5c869
Yoran_Furan <yon.yoran.f.2nd@gmail.com>
2025-06-27 23:30:41 +0900

    Update : 29.34

--- main.cs
+++ main.cs
@@ -55,8 +55,8 @@ namespace TownOfHost
         // ==========
         //Sorry for many Japanese comments.
         public const string PluginGuid = "com.kymario.townofhost-k";
-        public const string PluginVersion = "51.13.29.33";//ほんとはx.y.z表記にしたかったけどx.y.z.km.ks表記だと警告だされる
-        public const string PluginShowVersion = "51.13.29<sub>.33</sub>";
+        public const string PluginVersion = "51.13.29.34";//ほんとはx.y.z表記にしたかったけどx.y.z.km.ks表記だと警告だされる
+        public const string PluginShowVersion = "51.13.29<sub>.34</sub>";
         public const string ModVersion = ".29";//リリースver用バージョン変更
 
         /// 配布するデバッグ版なのであればtrue。リリース時にはfalseにすること。
