b07ed3ed54b71bfab9522cffd002fcbc439e45cb
Yoran_Furan <yon.yoran.f.2nd@gmail.com>
2025-07-02 20:21:44 +0900

    Change : 歩くことで溶けるように変更

--- Roles/Crewmate/Snowman.cs
+++ Roles/Crewmate/Snowman.cs
@@ -26,106 +26,91 @@ public sealed class Snowman : RoleBase
         player
     )
     {
-        StartVision = OptStartVision.GetFloat();
-        MinVison = OptMinVision.GetFloat();
-        isElectricalDontTime = OptIsElectricalDontTime.GetBool();
-        Meltvision = OptMeltvision.GetFloat();
-        TaskCompleatAddVision = OptTaskCompleatAddVision.GetFloat();
-        AllTaskcompMeltvision = OptAllTaskCompMeltvision.GetFloat();
-        AllTaskCompMelt = AllTaskcompMeltvision is not 0;
+        FirstVision = OptionFirstVision.GetFloat();
+        Minvision = OptionMinVision.GetFloat();
+        MeltedSteps = OptionMeltedSteps.GetInt();
+        ElectricalIgnoreMelt = OptionElectricalIgnoreMelt.GetBool();
 
-        //最小が最大を超えている！？
-        if (MinVison > StartVision)
-        {
-            Logger.Error("雪だるまちゃんのスタートが最小越えてるよっ", "SnowMan");
-            MinVison = 0;
-        }
-        SyncTimer = 0;
-        Vision = StartVision;
+        NowVision = FirstVision;
+        OldProportion = 0;
     }
+    static OptionItem OptionFirstVision; static float FirstVision;
+    static OptionItem OptionMinVision; static float Minvision;//最小の視野
+    static OptionItem OptionMeltedSteps; static float MeltedSteps;//完全に溶ける歩数 
+    static OptionItem OptionElectricalIgnoreMelt; static bool ElectricalIgnoreMelt;//停電中に解けない
 
     enum OptionName
     {
-        SnowmanStartVision,
+        SnowmanFirstVision,
         SnowmanMinVision,
-        SnowmanIsElectricalmode,
-        SnowmanMeltvision,
-        SnowmanAllTaskCompMeltvision,
-        SnowmanTaskCompAddvision,
-        SnowmanAllTaskMinVision
+        ElectricalIgnoreMelt,
+        SnowmanMeltedSteps
     }
-    //同期までのタイマー
-    float SyncTimer;
-    //現在の視界
-    float Vision;
-
-    //開始 , 上限の視界
-    static OptionItem OptStartVision; static float StartVision;
-
-    //最小の視界
-    static OptionItem OptMinVision; static float MinVison;
-
-    // 停電中なら溶けない処理
-    static OptionItem OptIsElectricalDontTime; static bool isElectricalDontTime;
-
-    // 1秒間に縮まる視界
-    static OptionItem OptMeltvision; static float Meltvision;
-
-    //タスクコンプで視界が減少し辛く　　　　　　 タスクコンプの時の減少量                 タスクコンプしても減少する
-    static OptionItem OptAllTaskCompMeltvision; static float AllTaskcompMeltvision; static bool AllTaskCompMelt;
-
-    //タスク完了時に広がる視界
-    static OptionItem OptTaskCompleatAddVision; static float TaskCompleatAddVision;
-
-
-    private static void SetupOptionItem()
+    static void SetupOptionItem()
     {
-        OptStartVision = FloatOptionItem.Create(RoleInfo, 10, OptionName.SnowmanStartVision, new(0.05f, 5, 0.05f), 0.6f, false).SetValueFormat(OptionFormat.Multiplier);
-        OptMinVision = FloatOptionItem.Create(RoleInfo, 11, OptionName.SnowmanMinVision, new(0f, 5, 0.05f), 0.15f, false).SetValueFormat(OptionFormat.Multiplier);
-        OptMeltvision = FloatOptionItem.Create(RoleInfo, 12, OptionName.SnowmanMeltvision, new(0.0005f, 0.005f, 0.0001f), 0.003f, false).SetValueFormat(OptionFormat.Multiplier);
-        OptIsElectricalDontTime = BooleanOptionItem.Create(RoleInfo, 13, OptionName.SnowmanIsElectricalmode, true, false);
-        OptTaskCompleatAddVision = FloatOptionItem.Create(RoleInfo, 14, OptionName.SnowmanTaskCompAddvision, new(0, 0.03f, 0.001f), 0.01f, false).SetValueFormat(OptionFormat.Multiplier);
-        OptAllTaskCompMeltvision = FloatOptionItem.Create(RoleInfo, 15, OptionName.SnowmanAllTaskCompMeltvision, new(0, 0.001f, 0.0001f), 0f, false).SetValueFormat(OptionFormat.Multiplier);
+        OptionFirstVision = FloatOptionItem.Create(RoleInfo, 10, OptionName.SnowmanFirstVision, new(0.05f, 5, 0.05f), 1.25f, false).SetValueFormat(OptionFormat.Multiplier);
+        OptionMinVision = FloatOptionItem.Create(RoleInfo, 11, OptionName.SnowmanMinVision, new(0f, 5, 0.05f), 0.15f, false).SetValueFormat(OptionFormat.Multiplier);
+        OptionMeltedSteps = FloatOptionItem.Create(RoleInfo, 12, OptionName.SnowmanMeltedSteps, new(100f, 3000f, 100f), 1800f, false);
+        OptionElectricalIgnoreMelt = BooleanOptionItem.Create(RoleInfo, 13, OptionName.ElectricalIgnoreMelt, true, false);
 
         OverrideTasksData.Create(RoleInfo, 20);
     }
-    public override void ApplyGameOptions(IGameOptions opt)
-    {
-        opt.SetFloat(FloatOptionNames.CrewLightMod, Vision);
-    }
-    public override bool OnCompleteTask(uint taskid)
-    {
-        if (!Player.IsAlive()) return true;
-
-        Vision += TaskCompleatAddVision;
-        Vision = Mathf.Min(StartVision, Vision);
-        Player.MarkDirtySettings();
-
-        return true;
-    }
+    float OldProportion;
+    float NowVision;
+    float stoptimer;
+    float NowWalkCount;//歩数計
+    Vector2 OldPosition;//前チェックの位置
     public override void OnFixedUpdate(PlayerControl player)
     {
-        //ホストじゃない,死んでる,会議中,イントロ終わってない,スポーンしてないならリターン
-        if (!AmongUsClient.Instance.AmHost || !player.IsAlive() || GameStates.Meeting || GameStates.Intro || !MyState.HasSpawned) return;
+        if (AmongUsClient.Instance.AmHost is false || player.IsAlive() is false) return;
+        if (MyTaskState.IsTaskFinished) return;
+        if (Utils.IsActive(SystemTypes.Electrical) && ElectricalIgnoreMelt) return;
 
-        //夜 (停電中なら進行しない)
-        if ((isElectricalDontTime && Utils.IsActive(SystemTypes.Electrical))
-        || (!AllTaskCompMelt && MyTaskState.IsTaskFinished)//タスクコンプで減少しない設定かつタスクコンプなら処理しない
-        || (Vision == MinVison) //そもそも視界が最小なら減少処理いらん
-        ) return;
-
-        //同期までのタイマー
-        SyncTimer += Time.fixedDeltaTime;
-
-        if (SyncTimer > 1f)
+        if (player.GetTruePosition() == OldPosition)
         {
-            //視界を減少させる   減少恩恵なら　こっち　　　　　　　　　　　　　　　　そうじゃないならこっち
-            Vision -= MyTaskState.IsTaskFinished ? AllTaskcompMeltvision : Meltvision;
-            //最小以下にはしない
-            Vision = Mathf.Max(MinVison, Vision);
-
-            player.MarkDirtySettings();
-            SyncTimer = 0;
+            stoptimer += Time.fixedDeltaTime;
+            if (stoptimer > 1)
+            {
+                NowWalkCount += 5;
+                stoptimer = 0;
+                CheckVision();
+            }
+            return;
         }
+        if (OldPosition == new Vector2(50f, 50f)
+        || Player.inVent || Player.MyPhysics.Animations.IsPlayingEnterVentAnimation()
+        || Player.MyPhysics.Animations.IsPlayingAnyLadderAnimation() || Player.inMovingPlat
+        || !Player.CanMove)//歩行以外は除外
+        {
+            OldPosition = player.GetTruePosition();
+            return;
+        }
+
+        var distance = Vector2.Distance(OldPosition, player.GetTruePosition());
+        OldPosition = player.GetTruePosition();
+        //歩数計の確認
+        NowWalkCount += distance;
+        stoptimer = 0;
+        CheckVision();
+    }
+    public void CheckVision()
+    {
+        var Proportion = NowWalkCount / MeltedSteps;
+        var MeltVision = FirstVision - Minvision;//減少を受ける視野
+        if (Proportion > 1) Proportion = 1;
+        var vision = (MeltVision - (MeltVision * Proportion)) + Minvision;
+
+        if (Proportion - OldProportion > 0.05f)//最大20回更新
+        {
+            OldProportion = Proportion;
+            Player.RpcProtectedMurderPlayer();
+            NowVision = vision;
+            Logger.Info($"減少率:{Proportion} 歩数:{NowWalkCount} , 現在の視野{NowVision}", "Snowman");
+            Player.MarkDirtySettings();
+        }
+    }
+    public override void ApplyGameOptions(IGameOptions opt)
+    {
+        opt.SetFloat(FloatOptionNames.CrewLightMod, NowVision);
     }
 }
