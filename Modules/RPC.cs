700a7ec37934ac52d00bd20c9944e8e82a0eb73d
BuildTools <unconfigured@null.spigotmc.org>
2025-07-30 06:41:22 +0900

    fix: バージョンチェック不安定ぽいのでまだ消しておく

--- Modules/RPC.cs
+++ Modules/RPC.cs
@@ -231,13 +231,13 @@ namespace TownOfHost
         }
         public static async void RpcVersionCheck()//クラッシュの原因。正味まだクライアント対応してないから消してもいい気はする
         {
-            while (PlayerControl.LocalPlayer == null) await Task.Delay(800);
+            while (PlayerControl.LocalPlayer == null) await Task.Delay(500); //800ぐらいで動く
 
-            MessageWriter writer = AmongUsClient.Instance.StartRpcImmediately(PlayerControl.LocalPlayer.NetId, (byte)CustomRPC.VersionCheck, SendOption.Reliable);
+            /*MessageWriter writer = AmongUsClient.Instance.StartRpcImmediately(PlayerControl.LocalPlayer.NetId, (byte)CustomRPC.VersionCheck, SendOption.Reliable);
             writer.Write(Main.PluginVersion);
             writer.Write($"{ThisAssembly.Git.Commit}({ThisAssembly.Git.Branch})");
             writer.Write(Main.ForkId);
-            AmongUsClient.Instance.FinishRpcImmediately(writer);
+            AmongUsClient.Instance.FinishRpcImmediately(writer);*/
             Main.playerVersion[PlayerControl.LocalPlayer.PlayerId] = new PlayerVersion(Main.PluginVersion, $"{ThisAssembly.Git.Commit}({ThisAssembly.Git.Branch})", Main.ForkId);
         }
         public static void RpcModUnload(byte playerId)
